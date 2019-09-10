07a1c5d9b87e90e21b3703da528cbcdb854a1043
Andres Carranza <diacarry@gmail.com>
2019-09-10 09:57:54 -0500

    Creacion de archivo y ruta para formulario

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -81,18 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PROGRACOL TEST
+                    <h6>Andres Carranza</h6>
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="">Haga Click Aquí</a>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/tickets', function () {
+    return view('ticket');
 });
