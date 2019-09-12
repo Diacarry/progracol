3f5cbb09a2091038a79ac773bb16ad369b8e868a
Andres Carranza <diacarry@gmail.com>
2019-09-12 10:27:35 -0500

    Redefiniendo valores por default de modelos (Nombre de la tabla, Primary_key, Primary_key auto-incrementable, tipo de dato de la Primary_key, Habilitar TimeStamps, Fotmato del TimeStamps por default y los nombres de variables del CREATED_AT Y UPDATED_AT)

--- app/TicketPriority.php
+++ app/TicketPriority.php
@@ -5,5 +5,47 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class TicketPriority extends Model {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'ticket_priorities';
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'priority_id';
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     * Options: true -> Auto-Increment; false -> No Auto-Increment
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    //protected $keyType = 'string';
+    /**
+     * Indicates if the model should be timestamped.
+     * Options: true -> able; false -> enable
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+    /**
+     * The storage format of the model's date columns.
+     *
+     * @var string
+     */
+    //protected $dateFormat = 'U';
+    /**
+     * Names variables CREATED_AT and UPDATED_AT (default)
+     */
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
 }
--- app/TicketTicket.php
+++ app/TicketTicket.php
@@ -5,6 +5,49 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class TicketTicket extends Model {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'ticket_tickets';
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'ticket_id';
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     * Options: true -> Auto-Increment; false -> No Auto-Increment
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    //protected $keyType = 'string';
+    /**
+     * Indicates if the model should be timestamped.
+     * Options: true -> able; false -> enable
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+    /**
+     * The storage format of the model's date columns.
+     *
+     * @var string
+     */
+    //protected $dateFormat = 'U';
+    /**
+     * Names variables CREATED_AT and UPDATED_AT (default)
+     */
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
     /*$user = App\TicketUser::find(1);
 
     foreach ($user->roles as $role) {
